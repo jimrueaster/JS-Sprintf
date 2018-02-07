d35c461c8e47261328e91b91d1c877358ba4f9a3
jimrueaster <jimru.easter@hotmail.com>
2018-02-07 10:16:42 +0800

    Update to ES2015 Edition

--- sprintf.js
+++ sprintf.js
@@ -1,6 +1,8 @@
 /*
 **	JS - sprintf()
-**	param: arg0, arg1, arg2, ...
+**	@param originString
+**	@param args
+**	@returns string
 **	description: Sprintf JS version
 					arg0 is the whole string; arg1, arg2, ..., argn are the short string
 **	E.g.: var str = "%s really wants to help %s.";
@@ -9,11 +11,10 @@
 **	author: Jimru Easter(295140325@qq.com)
 ** 	time: 2017-4-5 22:31:29
 */
-function sprintf(){
-	var originString = arguments[0];
-	
-	for(var i=1; i<= arguments.length; i++){
-		originString = originString.replace(/%s/, arguments[i]);
+let sprintf = (originString, ...args) => {
+
+	for(let i=0; i < args.length; i++){
+		originString = originString.replace(/%s/, args[i]);
 	}
 	return originString;
-}
+};
